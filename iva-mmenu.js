c419890a527799c322e9a6e549b5c99456e7a08a
femkhan <femkhan@users.noreply.github.com>
2014-11-01 03:30:19 +0530

    Update iva-mmenu.js

--- iva-mmenu.js
+++ iva-mmenu.js
@@ -1,19 +1,18 @@
 (function($){  "use strict";
 	// Define your library strictly...
 	jQuery(document).ready(function ($) {
+
+		jQuery('.iva-mobile-dropdown').click(function(){
+			jQuery('.iva-mobile-menu').slideToggle(500);
+			return false;
+		});
+        
+		// Child Menu Toggle
+		jQuery('.iva-children-indenter').click(function(){
+			jQuery(this).parent().parent().toggleClass('iva-menu-open');
+			jQuery(this).parent().parent().find('> ul').slideToggle();
+			return false;
+		});
 	
-        jQuery('.iva-mobile-dropdown').click(function(){
-        	jQuery('.iva-mobile-menu').slideToggle(500);
-        	return false;
-        });
-        
-        // Child Menu Toggle
-        jQuery('.iva-children-indenter').click(function(){
-        	jQuery(this).parent().parent().toggleClass('iva-menu-open');
-        	jQuery(this).parent().parent().find('> ul').slideToggle();
-        	
-        	return false;
-        });
-        
 	});       
 })();
